2861f37fa12bf4b4ad466ef2bee466d5b946345c
Fang-zhongbin <13969040600@163.com>
2016-07-04 14:28:36 +0800

    没怎么修改！ Signed-off-by: Fang-zhongbin <13969040600@163.com>

--- App/Home/Controller/UserController.class.php
+++ App/Home/Controller/UserController.class.php
@@ -5,10 +5,12 @@ use Think\Controller;
 
 class UserController extends Controller
 {
+	//注册页面
 	public function Register()
 	{
 		$this->display();
 	}
+	//登陆的方法
 	public function Login()
 	{
 		$this->display();
